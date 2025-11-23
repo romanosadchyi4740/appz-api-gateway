f1498cb8e7916d1ba98d86d49a5e841d2c8cf754
romanos4740 <romanos4740@gmail.com>
2025-11-23 23:54:33 +0200

    Updated the security configuration for api gateway

---
+++ src/main/java/com/romanosadchyi/labs/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.romanosadchyi.labs.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                // Disable CSRF (Cross-Site Request Forgery) as we are stateless
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+
+                // Allow ALL requests to pass through the security filter chain
+                // This is safe because your 'AuthenticationFilter' handles the actual security logic
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+}
